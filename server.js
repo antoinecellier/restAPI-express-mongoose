6a7ffdb9f8b97f9b81a73968ddff0e7920916ad5
cellier <antoine08.cellier@hotmail.fr>
2015-02-09 16:16:15 +0100

    port

--- server.js
+++ server.js
@@ -45,8 +45,12 @@ app.use(function(req, res, next) {
 });
 
 // Start the server
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+// var server = app.listen(app.get('port'), function() {
+//   console.log('Express server listening on port ' + server.address().port);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
